a0712e78f025a7a994807e378a2cfa5776efb002
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-02-26 22:03:53 +0100

    Use an heredoc for the page header

--- build.sh
+++ build.sh
@@ -8,11 +8,17 @@ git clone --depth=1 https://github.com/godotengine/godot.git "$TMP"
 
 # Generate a Markdown table of the class reference coverage.
 mkdir -p content/
-rm -f content/index.md
+rm -f content/_index.md
 
 # Add Git commit information to the generated page.
+# Must end with a blank line so the table that will be appended can be parsed correctly.
 COMMIT_HASH="$(git -C "$TMP" rev-parse --short=9 HEAD)"
-echo -e "Generated from commit [$COMMIT_HASH](https://github.com/godotengine/godot/commit/$COMMIT_HASH).\n" > content/_index.md
+cat << EOF > content/_index.md
+# Godot class reference status
+
+Generated from Godot commit [$COMMIT_HASH](https://github.com/godotengine/godot/commit/$COMMIT_HASH).
+
+EOF
 
 # Trim the first line of the output to get a valid Markdown table.
 python3 "$TMP/doc/tools/doc_status.py" -u "$TMP/doc/classes" | tail -n +2 >> content/_index.md
